77f304035d681b4625e4dfa829c18995d287aeff
kaushik94 <kaushik.varanasi1@gmail.com>
2015-02-23 22:43:40 +0530

    webmails should be deleted, figure out how

--- bs.py
+++ bs.py
@@ -2,7 +2,10 @@ from bs4 import BeautifulSoup as bss
 from mechanize import Browser
 import requests
 import re
-import html2text
+
+GMAIL_ID = 'your_gmail_id'
+WEBMAIL_ID = 'webmail id'
+WEBMAIL_PASS = 'webmail password'
 
 url = "webmail.daiict.ac.in"
 master = 'https://webmail.daiict.ac.in/'
@@ -11,8 +14,8 @@ br = Browser()
 br.set_handle_robots(False)
 br.open("https://webmail.daiict.ac.in/zimbra/")
 br.select_form(name='loginForm')
-br.form['username'] = '201201111'
-br.form['password'] = 'twinparadox'
+br.form['username'] = WEBMAIL_ID
+br.form['password'] = WEBMAIL_PASS
 br.submit()
 
 response = br.response().read()
@@ -20,34 +23,54 @@ pool = bss(response)
 links = []
 messages = pool.find_all('tbody', {'id':'mess_list_tbody'})[0]
 messages = messages.findAll('a',href=True)
-#print len(messages)
 prev = 'https://webmail.daiict.ac.in/zimbra/h/search?si=1&so=0&sc=4986&st=message&id='
 next = '&xim=1&action=paneView'
 
+
 for message in messages:
-        #print message
-        something, id_ = re.findall('id=(.*?)&', str(message))  
-        opened = br.open(prev+id_+next)
+        something, id_ = re.findall('id=(.*?)&', str(message))
+        message_link = prev+id_+next  
+        opened = br.open(message_link)
         response = br.response().read()
         response = bss(response)
         forward = response.findAll('a', {'id':'OPFORW'})
         if len(forward):
                 forward = forward[0]
                 link = forward['href']
-                #print link
-                cr = Browser()
-                cr.set_handle_robots(False)
-                cr.open(link)
+                br.open(link)
                 #print cr.response().read()
-                cr.select_form(name='composeForm')
+                br.select_form(name='composeForm')
                 #print cr.form
-                cr.form['to'] = 'kaushik.varanasi1@gmail.com'
+                br.form['to'] = GMAIL_ID
                 #print cr.form 
-                cr.submit()
-                print "done", id_
+                br.submit()
+                #print "done", id_
+                """
+                br.open(message_link)
+                br.select_form(nr=1)
+                br.form.set_all_readonly(False)
+                delete = br.find_control(id='SOPDELETE')
+                delete.checked = True
+                br.click(type='submit', id='SOPDELETE')
+                print delete
+                br.submit()
+                """
+
+"""
 r = br.open(master)
 r = bss(r.read())
 br.select_form(name='zform')
+br.form.set_all_readonly(False)
 check = br.find_control(id='OPCHALL')
-check.selected = True
+print check
+check.items[0].selected = True
+print check
+print "checked"
+delete = br.form.find_control(id='SOPDELETE')
+print delete
+#br.form.click(id='OPCHALL')
+print check
+br.click(id='SOPDELETE')
 br.submit()
+print "deleted"
+"""
